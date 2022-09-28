05a9fa441f7bfeb4ec3615de057c59cbc5b0e473
Jonathan <jonathanjacobovski@alunos.utfpr.edu.br>
2022-09-27 22:35:42 -0300

    Grafico

--- grafico.py
+++ grafico.py
@@ -4,15 +4,23 @@ from random import randrange
 randrange(0,11)
 
 notas_matematica = []
+notas_portugues = []
+notas_quimica = []
+notas_semestre = [notas_matematica, notas_portugues, notas_quimica]
+materias = ['Matemática','Português','Química']
 
 for notas in range(8):
     notas_matematica.append(randrange(0,11))
-print(notas_matematica)
+    notas_portugues.append(randrange(0,11))
+    notas_quimica.append(randrange(0,11))
 
-x = list(range(1, 9))
-y = notas_matematica
-plt.plot(x, y, marker='o')
-plt.title('Notas matemática')
-plt.xlabel('Provas')
-plt.ylabel('Notas')
-plt.show()
+i = -1
+for materia in notas_semestre:
+    i += 1
+    x = list(range(1, 9))
+    y = materia
+    plt.plot(x, y, marker='o')
+    plt.title(f'{materias[i]}')
+    plt.xlabel('Provas')
+    plt.ylabel('Notas')
+    plt.show()
